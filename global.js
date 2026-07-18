c20d9b1612dcab71cf907c8962007a4b62b86ee2
theformationparty <theformationparty@gmail.com>
2026-07-18 05:48:48 -0400

    Add files via upload

---
+++ global.js
@@ -0,0 +1,39 @@
+let btn_flag = true;
+let pause_flag = false;
+const btns = document.querySelectorAll("button");
+btns.forEach(element => {
+    element.addEventListener("mouseenter", () => {
+        pause_flag = true;
+        element.style.color = "rgba(178, 231, 204, 1)";
+        element.style.letterSpacing = "0.1rem";
+    });
+    element.addEventListener("mouseleave", () => {
+        pause_flag = false;
+        element.style.color = "rgba(255, 183, 0, 1)";
+    });
+});
+document.getElementById("text_logo").addEventListener("click", () => {
+    window.location.href = "https://rastaco.org/";
+});
+document.getElementById("rasta_nova_span").addEventListener("click", () => {
+    window.open("https://rastanova.org/");
+});
+function ButtonAnimation() {
+    btn_flag = !btn_flag;
+    if (!pause_flag) {
+        btns.forEach(element => {
+            if (btn_flag) {
+                element.style.letterSpacing = "0.1rem";
+                element.style.color = "rgba(255, 183, 0, 1)";
+            }
+            else {
+                element.style.letterSpacing = "0rem";
+                element.style.color = "rgba(178, 231, 204, 1)";
+            }
+        });
+    }
+    
+}
+setInterval(() => {
+    ButtonAnimation();
+}, Math.floor(Math.random() * 1000));
